df68ae1579fc65468a12ea4ff9b4e9ca8d74c0f0
imontalvomiguel <ubaldo.im@gmail.com>
2017-12-22 01:52:27 -0600

    Move order helpers to resource class

--- lib/shopify_api/resources/order.rb
+++ lib/shopify_api/resources/order.rb
@@ -11,4 +11,30 @@ class ShopifyAPI::Order
     discount_code.amount
   end
 
+  def refunds_for_line_item(line_item)
+    refunds_with_line_item = refunds_with_line_item(line_item)
+    refund_line_items = refund_line_items(line_item, refunds_with_line_item)
+    sum_refund_line_items(refund_line_items)
+  end
+
+  private
+
+    def refunds_with_line_item(line_item)
+      refunds.select do |refund|
+        refund.refund_line_items.any? { |refund_item| refund_item.line_item.id == line_item.id }
+      end
+    end
+
+    def refund_line_items(line_item, refunds_with_line_item)
+      refunds_with_line_item.map { |refund| find_refund_for_item(line_item, refund.refund_line_items) }
+    end
+
+    def find_refund_for_item(line_item, refund_line_items)
+      refund_line_items.detect { |refund_item| refund_item.line_item.id == line_item.id }
+    end
+
+    def sum_refund_line_items(refund_line_items)
+      refund_line_items.inject(0) { |sum, refund_item| sum + refund_item.quantity }
+    end
+
 end
