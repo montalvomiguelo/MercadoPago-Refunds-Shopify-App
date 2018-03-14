13f47fef9dc21ece5dab9cac4d691e4f5903289d
imontalvomiguel <ubaldo.im@gmail.com>
2018-03-13 18:43:23 -0600

    Test refund with mercadopago

--- test/app_test.rb
+++ test/app_test.rb
@@ -158,6 +158,44 @@ class TestApp < Minitest::Test
     assert last_response.content_type.include?('application/json')
   end
 
+  def test_new_refund_with_mercadopago
+    set_session
+
+    shop = mock()
+    shop.stubs(:mp_client_id).returns('23')
+    shop.stubs(:mp_client_secret).returns('qwer')
+
+    fake "https://api.mercadopago.com/oauth/token", {:method => :post, :body => load_fixture('mercadopago_auth.json')}
+    fake "https://api.mercadopago.com/collections/3535309354/refunds?access_token=APP_USR-23", {:method => :post, :body => load_fixture('refund.json')}
+    fake "https://#{@shop_name}/admin/orders/450789469.json", body: load_fixture('order.json')
+    fake "https://api.mercadopago.com/collections/search?external_reference=901414060&access_token=APP_USR-23", body: load_fixture('payment.json')
+    fake "https://#{@shop_name}/admin/orders/450789469/metafields/1352251342860.json", {:method => :put}
+    fake "https://#{@shop_name}/admin/orders/450789469.json", {:method => :put}
+    fake "https://#{@shop_name}/admin/orders/450789469/refunds.json", {:method => :post}
+    fake "https://#{@shop_name}/admin/orders/450789469/metafields.json", body: load_fixture('metafields.json')
+
+    App.any_instance.stubs(:current_shop).returns(shop)
+
+    payload = {
+      :refund => {
+        :shipping => { :full_refund => true },
+        :refund_line_items => [
+          {
+            :line_item_id => 466157049,
+            :quantity => 1
+          }
+        ],
+        :amount => 199
+      }
+    }
+
+    mp = MercadoPago.new('23', 'qwer')
+
+    MercadoPago.expects(:new).returns(mp)
+
+    post '/orders/450789469/refunds', payload.to_json
+  end
+
   private
 
     def set_session(shop = @shop_name, token = '1234')
