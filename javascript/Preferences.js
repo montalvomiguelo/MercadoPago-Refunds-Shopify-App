637ce16dc978e601d51724de48582537968dd38b
Montalvo Miguelo <montalvomiguelo@yahoo.com>
2018-04-09 01:33:11 -0500

    Create Preferences component

---
+++ javascript/Preferences.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Page } from '@shopify/polaris';
+
+function Preferences(props) {
+  return (
+    <Page
+      title="Preferences"
+    >
+      <p>Page content</p>
+    </Page>
+  );
+}
+
+export default Preferences;
