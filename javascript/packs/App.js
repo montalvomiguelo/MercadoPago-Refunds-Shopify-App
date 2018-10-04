c2e9ca8dd940eaaaf46ae7655273fdd1c6b717eb
imontalvomiguel <ubaldo.im@gmail.com>
2018-10-04 12:50:17 -0500

    Fix attach the easdk object to the context of root App component

--- javascript/packs/App.js
+++ javascript/packs/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { hot } from 'react-hot-loader';
-
-import { AppProvider } from '@shopify/polaris';
 
 import Refund from './Refund';
 import Home from './Home';
@@ -12,22 +11,26 @@ import {
   Route
 } from 'react-router-dom';
 
-function App(props) {
-  const { apiKey, shopOrigin } = window;
+class App extends Component {
+  // This line is very important! It tells React to attach the `easdk`
+  // object to `this.context` within your component.
+  static contextTypes = {
+    easdk: PropTypes.object,
+  };
 
-  return (
-    <AppProvider shopOrigin={shopOrigin} apiKey={apiKey}>
+  render() {
+    return (
       <BrowserRouter>
-        <div>
+        <React.Fragment>
           <Route exact path="/" component={Home} />
           <Route path="/order" render={({ location }) => (
             <Refund orderId={location.search.split('=').pop()} />
           )} />
           <Route path="/preferences" component={Preferences} />
-        </div>
+        </React.Fragment>
       </BrowserRouter>
-    </AppProvider>
-  );
-};
+    );
+  }
+}
 
 export default hot(module)(App);
--- javascript/packs/application.js
+++ javascript/packs/application.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/application.css';
 
+import { AppProvider } from '@shopify/polaris';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const shopOrigin = document.querySelector('meta[name="ShopOrigin"]').content;
+const apiKey = document.querySelector('meta[name="APIKey"]').content;
+const forceRedirect = !!document.querySelector('meta[name="ForceRedirect"]');
+
+ReactDOM.render(
+  <AppProvider shopOrigin={shopOrigin} apiKey={apiKey} forceRedirect={forceRedirect}>
+    <App />
+  </AppProvider>,
+  document.getElementById('root')
+);
